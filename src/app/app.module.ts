a18dbaba831d398a6b043dd45048b6be2c8c905e
seaneum <seaneum88@gmail.com>
2020-02-13 18:29:07 -0800

    added flex layout library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
